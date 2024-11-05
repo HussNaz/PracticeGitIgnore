14a7e90aca18eb8de0428c480ec0f2f59d87c26e
HussNaz <nazmulhassanc09@gmail.com>
2024-11-05 15:59:07 +0600

    My Task 3

--- src/PracticeGitIgnore.java
+++ src/PracticeGitIgnore.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PracticeGitIgnore {
     public static void main(String[] args) {
         System.out.println("Here will be my code");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter your name");
+            String str = br.readLine();
+            System.out.println("Enter your Project Name");
+            String str2 = br.readLine();
+            System.out.println(str+" "+str2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
